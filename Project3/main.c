6112ab5ced1c37d77b25a51f44ec7bfa53a5c3b2
user <user@DESKTOP-RH0EED9>
2023-07-15 10:57:37 +0900

    숫자 넣어서 계단 만들기

--- Project3/main.c
+++ Project3/main.c
@@ -2,15 +2,17 @@
  
 int main()
 {
-	int i, j;
-
-	for (i = 1; i <= 7; i++)
-	{
+	int i, j, t;
+	printf("줄을 입력하시요");	
+	scanf("%d",&t);
+	
+	for (i = 0; i <= t; i++)
+	{   
 		for (j = 1; j <= i; j++)
 		{
 			printf("*");
 		}
-		printf("\n");
+			printf("\n");
 	}
 
 		return 0;
